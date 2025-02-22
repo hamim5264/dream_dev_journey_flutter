a8ae076c7446479cbb3e85bd9a62abe1acf337f6
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2025-02-22 14:18:45 +0600

    module 12- task manager pre-practice all authentication completed.

--- lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_pre_practice/task_manager_practice/screens/onboarding/pin_verification_screen.dart
+++ lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_pre_practice/task_manager_practice/screens/onboarding/pin_verification_screen.dart
@@ -1,4 +1,6 @@
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_pre_practice/task_manager_practice/api/api_clients.dart';
 import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_pre_practice/task_manager_practice/style/task_styles.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_pre_practice/task_manager_practice/utility/task_utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:pin_code_fields/pin_code_fields.dart';
 
@@ -10,6 +12,34 @@ class PinVerificationScreen extends StatefulWidget {
 }
 
 class _PinVerificationScreenState extends State<PinVerificationScreen> {
+  Map<String, String> formValues = {"otp": ""};
+  bool isLoading = false;
+
+  inputOnChange(mapKey, textValue) {
+    setState(() {
+      formValues.update(mapKey, (value) => textValue);
+    });
+  }
+
+  formOnSubmit() async {
+    if (formValues["otp"]!.length != 6) {
+      taskAppErrorToast("6 digit pin required!");
+    } else {
+      setState(() {
+        isLoading = true;
+      });
+      String? emailAddress = await readUserData("EmailVerification");
+      bool res = await verifyOTPRequest(emailAddress, formValues["otp"]);
+      if (res == true) {
+        Navigator.pushNamed(context, "/setPassword");
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,44 +47,57 @@ class _PinVerificationScreenState extends State<PinVerificationScreen> {
         children: [
           taskAppScreenBackground(context),
           Container(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "PIN Verification",
-                  style: head1Text(colorDarkBlue),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "A 6 digit pin has been send to your mobile number",
-                  style: head6Text(colorLightGray),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                PinCodeTextField(
-                  appContext: context,
-                  length: 6,
-                  pinTheme: taskOTPStyle(),
-                  animationType: AnimationType.fade,
-                  animationDuration: Duration(milliseconds: 300),
-                  enableActiveFill: true,
-                  onCompleted: (v) {},
-                  onChanged: (value) {},
-                ),
-                Container(
-                  child: ElevatedButton(
-                    style: taskAppButtonStyle(),
-                    onPressed: () {},
-                    child: taskAppSuccessButtonChild("Verify"),
+            alignment: Alignment.center,
+            child: isLoading
+                ? (Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.green,
+                    ),
+                  ))
+                : SingleChildScrollView(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "PIN Verification",
+                          style: head1Text(colorDarkBlue),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "A 6 digit pin has been send to your mobile number",
+                          style: head6Text(colorLightGray),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        PinCodeTextField(
+                          appContext: context,
+                          length: 6,
+                          pinTheme: taskOTPStyle(),
+                          animationType: AnimationType.fade,
+                          animationDuration: Duration(milliseconds: 300),
+                          enableActiveFill: true,
+                          onCompleted: (v) {},
+                          onChanged: (value) {
+                            inputOnChange("otp", value);
+                          },
+                        ),
+                        Container(
+                          child: ElevatedButton(
+                            style: taskAppButtonStyle(),
+                            onPressed: () {
+                              formOnSubmit();
+                            },
+                            child: taskAppSuccessButtonChild("Verify"),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
           ),
         ],
       ),
