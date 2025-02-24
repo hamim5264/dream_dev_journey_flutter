cb9eb974d517b3bc24edb44e7450bacffa493c6b
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2025-02-24 15:44:44 +0600

    module 14- task manager Live class- task all screens api, task update, delete api completed.

--- lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/screens/cancelled_task_screen.dart
+++ lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/screens/cancelled_task_screen.dart
@@ -1,3 +1,7 @@
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/models/task_list_model.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/network_caller/network_caller.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/network_caller/network_response.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/utilities/urls.dart';
 import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/widgets/profile_summary_card.dart';
 import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/widgets/task_item_card.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,31 @@ class CancelledTaskScreen extends StatefulWidget {
 }
 
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
+  bool getCancelledTaskInProgress = false;
+  TaskListModel taskListModel = TaskListModel();
+
+  Future<void> getCancelledTaskList() async {
+    getCancelledTaskInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    final NetworkResponse response =
+        await NetworkCaller().getRequest(Urls.getCancelledTasks);
+    if (response.isSuccess) {
+      taskListModel = TaskListModel.fromJson(response.jsonResponse);
+    }
+    getCancelledTaskInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCancelledTaskList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +47,30 @@ class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
           children: [
             ProfileSummaryCard(),
             Expanded(
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return const TaskItemCard();
-                },
+              child: Visibility(
+                visible: getCancelledTaskInProgress == false,
+                replacement: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.green,
+                  ),
+                ),
+                child: ListView.builder(
+                  itemCount: taskListModel.taskList?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    return TaskItemCard(
+                      task: taskListModel.taskList![index],
+                      onStatusChange: () {
+                        getCancelledTaskList();
+                      },
+                      showProgress: (inProgress) {
+                        getCancelledTaskInProgress = inProgress;
+                        if (mounted) {
+                          setState(() {});
+                        }
+                      },
+                    );
+                  },
+                ),
               ),
             ),
           ],
--- lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/screens/progress_task_screen.dart
+++ lib/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/screens/progress_task_screen.dart
@@ -1,3 +1,7 @@
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/models/task_list_model.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/network_caller/network_caller.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/network_caller/network_response.dart';
+import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/data/utilities/urls.dart';
 import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/widgets/profile_summary_card.dart';
 import 'package:dream_dev_journey_flutter/module12_13_14_flutter_task_manager_project_using_rest_api/module12_13_14_live_class/ui/widgets/task_item_card.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,31 @@ class ProgressTaskScreen extends StatefulWidget {
 }
 
 class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  bool getProgressTaskInProgress = false;
+  TaskListModel taskListModel = TaskListModel();
+
+  Future<void> getProgressTaskList() async {
+    getProgressTaskInProgress = true;
+    if (mounted) {
+      setState(() {});
+    }
+    final NetworkResponse response =
+        await NetworkCaller().getRequest(Urls.getProgressTasks);
+    if (response.isSuccess) {
+      taskListModel = TaskListModel.fromJson(response.jsonResponse);
+    }
+    getProgressTaskInProgress = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProgressTaskList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +48,30 @@ class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
             children: [
               ProfileSummaryCard(),
               Expanded(
-                child: ListView.builder(
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return const TaskItemCard();
-                  },
+                child: Visibility(
+                  visible: getProgressTaskInProgress == false,
+                  replacement: const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.green,
+                    ),
+                  ),
+                  child: ListView.builder(
+                    itemCount: taskListModel.taskList?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      return TaskItemCard(
+                        task: taskListModel.taskList![index],
+                        onStatusChange: () {
+                          getProgressTaskList();
+                        },
+                        showProgress: (inProgress) {
+                          getProgressTaskInProgress = inProgress;
+                          if (mounted) {
+                            setState(() {});
+                          }
+                        },
+                      );
+                    },
+                  ),
                 ),
               ),
             ],
