75dfe0cc676745ba5e7d4481d5c4dd97072b6f55
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2025-02-28 11:15:25 +0600

    module 16- flutter firebase class3: firebase push notification/messaging.

--- lib/module16_flutter_firebase/firebase_app/firebase_student_app.dart
+++ lib/module16_flutter_firebase/firebase_app/firebase_student_app.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dream_dev_journey_flutter/module16_flutter_firebase/firebase_messaging_service/firebase_messaging_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessagingService().initialize();
+  await FirebaseMessagingService().getFCMToken();
+  await FirebaseMessagingService().subscribeToTopic("Dream-Dev-Journey");
+  await FirebaseMessagingService().onRefresh((token) {
+    /// Send to api
+  });
   runApp(
     FirebaseApplication(),
   );
@@ -41,10 +48,10 @@ class _OstadStudentListState extends State<OstadStudentList> {
   Future<void> getStudentsData() async {
     studentList.clear();
     final QuerySnapshot result = await studentCollectionRef.get();
-    print(result.size);
+    //print(result.size);
     for (QueryDocumentSnapshot element in result.docs) {
-      print(element.data());
-      print(element.get("name"));
+      //print(element.data());
+      //print(element.get("name"));
       OstadStudents ostadStudents = OstadStudents(element.get("name"),
           int.tryParse(element.get("roll").toString()) ?? 0, element.id);
       studentList.add(ostadStudents);
