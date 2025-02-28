2030483d43dad7609f0433d31f6f9b103d003b22
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2025-02-28 17:29:12 +0600

    module 17- flutter google map current location completed.

---
+++ lib/module17_flutter_location_and_map_widgets/module17_assignment/assignment_current_location_main.dart
@@ -0,0 +1,18 @@
+import 'package:dream_dev_journey_flutter/module17_flutter_location_and_map_widgets/module17_assignment/current_location_home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MapScreen(),
+    );
+  }
+}
