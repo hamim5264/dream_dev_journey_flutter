2bf2557395ad35a38f09dcd8ffebd6d15fe728c6
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2025-03-02 17:24:28 +0600

    module 18- live class-1: E-commerce project theme configuration, splash screen and verify email screen completed.

---
+++ lib/module18_22_flutter_ecommerce_project/presentation/ui/screens/splash_screen.dart
@@ -0,0 +1,52 @@
+import 'package:dream_dev_journey_flutter/module18_22_flutter_ecommerce_project/presentation/ui/screens/verify_email_screen.dart';
+import 'package:dream_dev_journey_flutter/module18_22_flutter_ecommerce_project/presentation/ui/utility/assets_path.dart';
+import 'package:dream_dev_journey_flutter/module18_22_flutter_ecommerce_project/presentation/ui/widgets/app_logo.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    moveToNextScreen();
+  }
+
+  Future<void> moveToNextScreen() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    Get.offAll(const VerifyEmailScreen());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            const AppLogo(),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const SizedBox(
+              height: 16,
+            ),
+            const Text(
+              "Version 1.0.0",
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/module18_22_flutter_ecommerce_project/presentation/ui/screens/verify_email_screen.dart
@@ -0,0 +1,66 @@
+import 'package:dream_dev_journey_flutter/module18_22_flutter_ecommerce_project/presentation/ui/widgets/app_logo.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailScreen extends StatefulWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  State<VerifyEmailScreen> createState() => _VerifyEmailScreenState();
+}
+
+class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 160,
+              ),
+              const AppLogo(
+                height: 80,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Welcome back",
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                "Please enter your email address",
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Email Address",
+                ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Next",
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
